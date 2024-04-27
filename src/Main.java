4fc1372a01940f65f470107411f11f1561b67385
nikce <git@github.com:kolya-shevtzov/helloworld.git>
2024-04-28 01:49:25 +0300

    string

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       String firstName ="Ivan ";
+       String middleName ="Ivanovich ";
+       String lastName = "Ivanov ";
+       String fullName =lastName + firstName + middleName;
+        System.out.println("Ф. И. О. сотрудника —"+fullName);
+
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — "+fullName.toUpperCase());
+
+        String fullName2= "Иванов Семён Семёнович";
+        fullName2=fullName2.replace("ё","е");
+        System.out.println("Данные Ф. И. О. сотрудника —"+fullName2);
     }
 }
